605f86b065caee91ed889a553e2bfcba6587cc1a
Tobias <tobias.isnan@gmail.com>
2019-02-14 10:11:52 +0000

    Added tempfile

---
+++ tempfile/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".removalButton").click(function(){
+    $(this).parent().remove();
+  });
+});
+
+$(document).ready(function(){
+  $("#addButton").click(function(){
+    alert("Dead end!!!");
+  });
+});
